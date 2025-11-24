143cd2ee2bdce7d84a281a5ba3f2e24b1e1a5ad4
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-11-24 19:11:13 +0530

    hotel management system

--- src/Reception.java
+++ src/Reception.java
@@ -217,6 +217,7 @@ public class Reception extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try{
+                    new SearchRoom();
 
                 }catch (Exception E){
                     E.printStackTrace();
